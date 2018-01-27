df886e20552feae9b79437776a0074b548f529c6
andyrobert3 <andyrobert3@users.noreply.github.com>
2018-01-27 16:17:49 +0800

    Shift to ultimate screen from main activity

--- app/src/main/java/com/example/aaryam123/everyday_challenge_final/MainActivity.java
+++ app/src/main/java/com/example/aaryam123/everyday_challenge_final/MainActivity.java
@@ -2,31 +2,38 @@ package com.example.aaryam123.everyday_challenge_final;
 
 import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    private ConstraintLayout constraintLayout;
+    private EditText name;
+    private Button saveNameButton;
+    private String nameOfUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        constraintLayout = findViewById(R.id.constraint_layout);
+        saveNameButton = findViewById(R.id.saveName);
+        name = findViewById(R.id.userName);
 
-        View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+        View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
+            public void onClick(View view) {
+                nameOfUser = name.getText().toString();
                 Intent i = new Intent(MainActivity.this, Ultimate_Activity.class);
+                i.putExtra("USERNAME", nameOfUser);
                 startActivity(i);
-                return true;
             }
         };
 
-        constraintLayout.setOnTouchListener(onTouchListener);
+        saveNameButton.setOnClickListener(onClickListener);
 
     }
 }
