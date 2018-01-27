9e32a2b3b87bcada3d2ba72585de8551cc448068
andyrobert3 <andyrobert3@users.noreply.github.com>
2018-01-27 20:33:10 +0800

    Checked the levels in Ultimate Activity, to make sure user cannot access levels that he isn't high enuf

--- app/src/main/java/com/example/aaryam123/everyday_challenge_final/Levels.java
+++ app/src/main/java/com/example/aaryam123/everyday_challenge_final/Levels.java
@@ -1,37 +1,41 @@
 package com.example.aaryam123.everyday_challenge_final;
 
-import java.io.IOException;
-
 /**
  * Created by alexa on 1/27/2018.
  */
 
 public class Levels {
-    public final static int MAX_LEVEL = 9;
+    public final static int TOTAL_LEVELS = 9;
 
     private int currLevel;
+    private int maxLevel; // personal max level
     // set number of challenges for levelChallenges array
     private int levelChallenges[] = {4,5,6,7,8,9,10,11,12};
     private int doneActivites[] ={0};
 
     Levels () {
         currLevel = 1;
+        maxLevel = 1;
     }
 
     // call when one is done with challenge at a level (1, 2, ... )
     public void doneChallenge(int level) {
-        if (level > MAX_LEVEL)
+        if (level > TOTAL_LEVELS)
             throw new IllegalArgumentException("Level accessed is too high");
 
-        if (doneActivites[level-1] >= levelChallenges[level-1])
-            currLevel = level+1;
-        else
+        if (doneActivites[level-1] >= levelChallenges[level-1]) {// exceed or equal to number of challenges at level
+            maxLevel = level + 1;
+            currLevel = level + 1;
+        } else {
+            maxLevel = level;
             currLevel = level;
+        }
 
-        doneActivites[currLevel-1]++;
+        doneActivites[maxLevel-1]++;
     }
 
     public int getCurrLevel() {
         return currLevel;
     }
+
 }
