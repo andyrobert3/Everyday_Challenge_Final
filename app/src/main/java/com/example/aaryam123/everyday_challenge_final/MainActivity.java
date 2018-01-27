7371003cb24a9a8014d84e10f94160ac48aec655
andyrobert3 <andyrobert3@users.noreply.github.com>
2018-01-27 14:11:53 +0800

    Did some design on the intro screen

--- app/src/main/java/com/example/aaryam123/everyday_challenge_final/MainActivity.java
+++ app/src/main/java/com/example/aaryam123/everyday_challenge_final/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.aaryam123.everyday_challenge_final;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View.OnTouchListener onTouchListener = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        };
+
     }
 }
