0defa04a8c5e6797729ce311357c83a429981836
Aaryam Srivastava <aaryamps1@gmail.com>
2018-01-28 01:51:50 +0800

    Added fb and whatsapp icons

--- app/src/main/java/com/example/aaryam123/everyday_challenge_final/profile.java
+++ app/src/main/java/com/example/aaryam123/everyday_challenge_final/profile.java
@@ -2,6 +2,9 @@ package com.example.aaryam123.everyday_challenge_final;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class profile extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
     }
 }
