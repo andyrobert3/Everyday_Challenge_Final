b0514594048906d69f36f11ac2e5e166db501979
andyrobert3 <andyrobert3@users.noreply.github.com>
2018-01-27 15:12:54 +0800

    Shift to ultimate screen from main activity

--- app/src/main/java/com/example/aaryam123/everyday_challenge_final/MainActivity.java
+++ app/src/main/java/com/example/aaryam123/everyday_challenge_final/MainActivity.java
@@ -1,23 +1,32 @@
 package com.example.aaryam123.everyday_challenge_final;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        constraintLayout = findViewById(R.id.constraint_layout);
+
         View.OnTouchListener onTouchListener = new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
+                Intent i = new Intent(MainActivity.this, Ultimate_Activity.class);
+                startActivity(i);
                 return true;
             }
         };
+
+        constraintLayout.setOnTouchListener(onTouchListener);
 
     }
 }
