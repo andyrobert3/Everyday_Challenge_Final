dd29d563f21dd8ef37cc0c01027188fc41fe3218
Kristoporus <nkristoporus@gmail.com>
2018-01-27 16:40:29 +0800

    ultimate change

--- app/src/main/java/com/example/aaryam123/everyday_challenge_final/Ultimate_Activity.java
+++ app/src/main/java/com/example/aaryam123/everyday_challenge_final/Ultimate_Activity.java
@@ -1,49 +1,41 @@
 package com.example.aaryam123.everyday_challenge_final;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Ultimate_Activity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ultimate_);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
         TextView helloText = (TextView) findViewById(R.id.helloText);
-        TextView nameText = (TextView) findViewById(R.id.nameText);
+
+
+        String newString;
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                newString= null;
+            } else {
+                newString= extras.getString("USERNAME");
+            }
+        } else {
+            newString= (String) savedInstanceState.getSerializable("USERNAME");
+        }
+        TextView usernameText = (TextView) findViewById(R.id.usernameText);
+        usernameText.setText(newString);
+
         Button changeChallengeBtn = (Button) findViewById(R.id.changeChallengeBtn);
+        TextView currChallengeText = (TextView) findViewById(R.id.currChallengeText);
     }
 
 }
